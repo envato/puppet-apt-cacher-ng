978e44e18168607104b1a2443974e14ced73c731
David Goodlad <david@goodlad.net>
2013-09-01 07:43:26 +1200

    Make config source configurable as a param

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,6 @@
 class apt-cacher-ng (
-  $version = 'installed'
+  $version = 'installed',
+  $config_source = 'puppet:///modules/apt-cacher-ng/acng.conf',
 ) {
   package { 'apt-cacher-ng':
     ensure => $version,
@@ -13,7 +14,7 @@ class apt-cacher-ng (
   file { '/etc/apt-cacher-ng/acng.conf':
     source => ["puppet:///modules/site-apt-cacher-ng/${::fqdn}/acng.conf",
                'puppet:///modules/site-apt-cacher-ng/acng.conf',
-               'puppet:///modules/apt-cacher-ng/acng.conf'],
+               $config_source],
     notify  => Service['apt-cacher-ng'],
     require => Package['apt-cacher-ng'],
   }
